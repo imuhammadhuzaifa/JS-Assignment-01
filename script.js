6d621d8cdced1c8af3b1bedb0e3ea458a122e5c9
iMuhammadHuzaifa <itshuzaifa09@gmail.com>
2025-02-14 07:20:53 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+// Chapter 01 alerts
+
+// Qno 01: 
+
+alert("Hello World");
+
+// Qno 02:
+
+alert("Error! Please enter a valid password.");
+
+// Qno 03:
+
+alert("Welcome to JS Land... \nHappy Coding!");
+
+// Qno 04:
+
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+// Qno 05:
+
+alert("Hello... I can run JS through my web browser's console");
+
+// Qno 06:
+
+alert("Hello... I can run JS through my web browser's console");
+
+// Chapter 02 variables for strings
+
+// Qno 01:
+
+var username;
+
+// Qno 02:
+
+var myName = "Muhammad Huzaifa";
+
+// Qno 03:
+
+var message = "Hello World";
+alert(message);
+
+// Qno 04:
+
+var name = "Jhone Doe";
+var age = "15 years old";
+var course = "Certified Mobile Application Development";
+alert(name);
+alert(age);
+alert(course);
+
+// Qno 05:
+
+var food = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+alert(food);
+
+// Qno 06:
+
+var email = "example@gmail.com";
+alert("My email address is " + email);
+
+// Qno 07:
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// Qno 08:
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+// Qno 09:
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+// Chapter 03 variables for numbers
+
+// Qno 10: 
+
+var age = 15;
+
+alert("I am " + age + " years old");
+
+// Qno 11:
+
+var num = 14;
+
+alert("You have visited this site " + num + " times");
+
+// Qno 12:
+
+var year = 1990;
+document.write("My birthday year is " + year );
+document.write("<br>");
+document.write(" Data type of my declared variable is a number");
